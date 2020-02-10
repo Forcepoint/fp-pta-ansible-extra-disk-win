65bed2dec38414ea1a12901f366a1193c023927f
Jeremy Cornett <jeremy.cornett@forcepoint.com>
2020-02-10 13:52:49 -0700

    Try setting up the secondary disk by using powershell dsc directly instead of through the ansible module.

---
+++ templates/setup_secondary_disk.ps1
@@ -0,0 +1,23 @@
+Configuration WaitForDisk_InitializeDataDisk
+{
+    Import-DSCResource -ModuleName StorageDsc
+
+    Node localhost
+    {
+        WaitForDisk Disk{{ extra_disk_win_disk_id }}
+        {
+             DiskId = {{ extra_disk_win_disk_id }}
+             RetryIntervalSec = 60
+             RetryCount = 60
+        }
+
+        Disk {{ extra_disk_win_drive_letter }}Volume
+        {
+             DiskId = {{ extra_disk_win_disk_id }}
+             DriveLetter = '{{ extra_disk_win_drive_letter }}'
+             FSLabel = '{{ extra_disk_win_drive_name }}'
+             FSFormat = '{{ extra_disk_win_fs_format }}'
+             DependsOn = '[WaitForDisk]Disk{{ extra_disk_win_disk_id }}'
+        }
+    }
+}
